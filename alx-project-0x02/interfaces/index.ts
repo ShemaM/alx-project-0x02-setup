0727cbfccd0038427a8af70eb8c3dcee077adc96
Shema Manasseh <shemamanase992@gmail.com>
2025-11-21 00:04:14 +0300

    updated

---
+++ alx-project-0x02/interfaces/index.ts
@@ -0,0 +1,35 @@
+﻿// User interface example
+export interface User {
+  id: number;
+  name: string;
+  email: string;
+}
+
+// Project interface example
+export interface Project {
+  id: number;
+  title: string;
+  description: string;
+  completed: boolean;
+}
+
+// API Response interface example
+export interface ApiResponse<T> {
+  data: T;
+  message: string;
+  success: boolean;
+}
+
+// Navigation Item interface
+export interface NavItem {
+  label: string;
+  href: string;
+  icon?: string;
+}
+
+// Contact Form Data interface
+export interface ContactFormData {
+  name: string;
+  email: string;
+  message: string;
+}
